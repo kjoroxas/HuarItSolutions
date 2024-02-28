eaf7ead3c44d49e5e3a87b9d8dd94748812e37c0
DarChar <darrenmendoza81@gmail.com>
2024-02-28 09:33:42 +0800

    load testing for sales summary and winning transactions

--- Testing/SalesMenufiles/Sales Summary.aspx.designer.cs
+++ Testing/SalesMenufiles/Sales Summary.aspx.designer.cs
@@ -69,13 +69,13 @@ namespace HuarITSolutions.SalesMenufiles
         protected global::System.Web.UI.WebControls.Button Button2;
 
         /// <summary>
-        /// Button5 control.
+        /// pdfButton control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Button Button5;
+        protected global::System.Web.UI.WebControls.Button pdfButton;
 
         /// <summary>
         /// Button6 control.
